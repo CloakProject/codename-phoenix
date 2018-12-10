e184843d566569c0ec2ceaa3f66a744ba081d0b6
raizor <raizor@gmail.com>
2018-12-10 20:45:21 +0000

    Update hash.h to include X13 functionality.

--- src/hash.h
+++ src/hash.h
@@ -16,14 +16,142 @@
 #include <version.h>
 
 #include <string>
+// X13
+#include "crypto/sph_blake.h"
+#include "crypto/sph_bmw.h"
+#include "crypto/sph_cubehash.h"
+#include "crypto/sph_echo.h"
+#include "crypto/sph_fugue.h"
+#include "crypto/sph_groestl.h"
+#include "crypto/sph_hamsi.h"
+#include "crypto/sph_jh.h"
+#include "crypto/sph_keccak.h"
+#include "crypto/sph_luffa.h"
+#include "crypto/sph_shavite.h"
+#include "crypto/sph_simd.h"
+#include "crypto/sph_skein.h"
+
 #include <vector>
 
 typedef uint256 ChainCode;
 
+#ifdef GLOBALDEFINED
+#define GLOBAL
+#else
+#define GLOBAL extern
+#endif
+GLOBAL sph_blake512_context z_blake;
+GLOBAL sph_bmw512_context z_bmw;
+GLOBAL sph_groestl512_context z_groestl;
+GLOBAL sph_jh512_context z_jh;
+GLOBAL sph_keccak512_context z_keccak;
+GLOBAL sph_skein512_context z_skein;
+GLOBAL sph_luffa512_context z_luffa;
+GLOBAL sph_cubehash512_context z_cubehash;
+GLOBAL sph_shavite512_context z_shavite;
+GLOBAL sph_simd512_context z_simd;
+GLOBAL sph_echo512_context z_echo;
+GLOBAL sph_hamsi512_context z_hamsi;
+GLOBAL sph_fugue512_context z_fugue;
+#define fillz()                            \
+    do {                                   \
+        sph_blake512_init(&z_blake);       \
+        sph_bmw512_init(&z_bmw);           \
+        sph_groestl512_init(&z_groestl);   \
+        sph_jh512_init(&z_jh);             \
+        sph_keccak512_init(&z_keccak);     \
+        sph_skein512_init(&z_skein);       \
+        sph_luffa512_init(&z_luffa);       \
+        sph_cubehash512_init(&z_cubehash); \
+        sph_shavite512_init(&z_shavite);   \
+        sph_simd512_init(&z_simd);         \
+        sph_echo512_init(&z_echo);         \
+        sph_hamsi512_init(&z_hamsi);       \
+        sph_fugue512_init(&z_fugue);       \
+    \
+} while (0)
+#define ZBLAKE (memcpy(&ctx_blake, &z_blake, sizeof(z_blake)))
+#define ZBMW (memcpy(&ctx_bmw, &z_bmw, sizeof(z_bmw)))
+#define ZGROESTL (memcpy(&ctx_groestl, &z_groestl, sizeof(z_groestl)))
+#define ZJH (memcpy(&ctx_jh, &z_jh, sizeof(z_jh)))
+#define ZKECCAK (memcpy(&ctx_keccak, &z_keccak, sizeof(z_keccak)))
+#define ZSKEIN (memcpy(&ctx_skein, &z_skein, sizeof(z_skein)))
+#define ZHAMSI (memcpy(&ctx_hamsi, &z_hamsi, sizeof(z_hamsi)))
+#define ZFUGUE (memcpy(&ctx_fugue, &z_fugue, sizeof(z_fugue)))
+template <typename T1>
+inline uint256 HashX13(const T1 pbegin, const T1 pend)
+{
+    sph_blake512_context ctx_blake;
+    sph_bmw512_context ctx_bmw;
+    sph_groestl512_context ctx_groestl;
+    sph_jh512_context ctx_jh;
+    sph_keccak512_context ctx_keccak;
+    sph_skein512_context ctx_skein;
+    sph_luffa512_context ctx_luffa;
+    sph_cubehash512_context ctx_cubehash;
+    sph_shavite512_context ctx_shavite;
+    sph_simd512_context ctx_simd;
+    sph_echo512_context ctx_echo;
+    sph_hamsi512_context ctx_hamsi;
+    sph_fugue512_context ctx_fugue;
+    static unsigned char pblank[1];
+
+    uint512 hash[17];
+    sph_blake512_init(&ctx_blake);
+    sph_blake512(&ctx_blake, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
+    sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[0]));
+
+    sph_bmw512_init(&ctx_bmw);
+    sph_bmw512(&ctx_bmw, static_cast<const void*>(&hash[0]), 64);
+    sph_bmw512_close(&ctx_bmw, static_cast<void*>(&hash[1]));
+    sph_groestl512_init(&ctx_groestl);
+    sph_groestl512(&ctx_groestl, static_cast<const void*>(&hash[1]), 64);
+    sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[2]));
+    sph_skein512_init(&ctx_skein);
+    sph_skein512(&ctx_skein, static_cast<const void*>(&hash[2]), 64);
+    sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[3]));
+
+    sph_jh512_init(&ctx_jh);
+    sph_jh512(&ctx_jh, static_cast<const void*>(&hash[3]), 64);
+    sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[4]));
+
+    sph_keccak512_init(&ctx_keccak);
+    sph_keccak512(&ctx_keccak, static_cast<const void*>(&hash[4]), 64);
+    sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[5]));
+    sph_luffa512_init(&ctx_luffa);
+    sph_luffa512(&ctx_luffa, static_cast<void*>(&hash[5]), 64);
+    sph_luffa512_close(&ctx_luffa, static_cast<void*>(&hash[6]));
+
+    sph_cubehash512_init(&ctx_cubehash);
+    sph_cubehash512(&ctx_cubehash, static_cast<const void*>(&hash[6]), 64);
+    sph_cubehash512_close(&ctx_cubehash, static_cast<void*>(&hash[7]));
+
+    sph_shavite512_init(&ctx_shavite);
+    sph_shavite512(&ctx_shavite, static_cast<const void*>(&hash[7]), 64);
+    sph_shavite512_close(&ctx_shavite, static_cast<void*>(&hash[8]));
+
+    sph_simd512_init(&ctx_simd);
+    sph_simd512(&ctx_simd, static_cast<const void*>(&hash[8]), 64);
+    sph_simd512_close(&ctx_simd, static_cast<void*>(&hash[9]));
+    sph_echo512_init(&ctx_echo);
+    sph_echo512(&ctx_echo, static_cast<const void*>(&hash[9]), 64);
+    sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[10]));
+    sph_hamsi512_init(&ctx_hamsi);
+    sph_hamsi512(&ctx_hamsi, static_cast<const void*>(&hash[10]), 64);
+    sph_hamsi512_close(&ctx_hamsi, static_cast<void*>(&hash[11]));
+    sph_fugue512_init(&ctx_fugue);
+    sph_fugue512(&ctx_fugue, static_cast<const void*>(&hash[11]), 64);
+    sph_fugue512_close(&ctx_fugue, static_cast<void*>(&hash[12]));
+    return hash[12].trim256();
+}
+/* ----------- Bitcoin Hash ------------------------------------------------- */
+
 /** A hasher class for Bitcoin's 256-bit hash (double SHA-256). */
-class CHash256 {
+class CHash256
+{
 private:
     CSHA256 sha;
+
 public:
     static const size_t OUTPUT_SIZE = CSHA256::OUTPUT_SIZE;
 
@@ -39,16 +167,19 @@ public:
         return *this;
     }
 
-    CHash256& Reset() {
+    CHash256& Reset()
+    {
         sha.Reset();
         return *this;
     }
 };
 
 /** A hasher class for Bitcoin's 160-bit hash (SHA-256 + RIPEMD-160). */
-class CHash160 {
+class CHash160
+{
 private:
     CSHA256 sha;
+
 public:
     static const size_t OUTPUT_SIZE = CRIPEMD160::OUTPUT_SIZE;
 
@@ -64,7 +195,8 @@ public:
         return *this;
     }
 
-    CHash160& Reset() {
+    CHash160& Reset()
+    {
         sha.Reset();
         return *this;
     }
@@ -104,14 +236,15 @@ private:
 
     const int nType;
     const int nVersion;
-public:
 
+public:
     CHashWriter(int nTypeIn, int nVersionIn) : nType(nTypeIn), nVersion(nVersionIn) {}
 
     int GetType() const { return nType; }
     int GetVersion() const { return nVersion; }
 
-    void write(const char *pch, size_t size) {
+    void write(const char* pch, size_t size)
+    {
         ctx.Write((const unsigned char*)pch, size);
     }
 
@@ -153,7 +286,7 @@ public:
 };
 
 /** Reads data from an underlying stream, while hashing the read data. */
-template<typename Source>
+template <typename Source>
 class CHashVerifier : public CHashWriter
 {
 private:
@@ -178,7 +311,7 @@ public:
         }
     }
 
-    template<typename T>
+    template <typename T>
     CHashVerifier<Source>& operator>>(T&& obj)
     {
         // Unserialize from this stream
@@ -188,8 +321,8 @@ public:
 };
 
 /** Compute the 256-bit hash of an object's serialization. */
-template<typename T>
-uint256 SerializeHash(const T& obj, int nType=SER_GETHASH, int nVersion=PROTOCOL_VERSION)
+template <typename T>
+uint256 SerializeHash(const T& obj, int nType = SER_GETHASH, int nVersion = PROTOCOL_VERSION)
 {
     CHashWriter ss(nType, nVersion);
     ss << obj;
