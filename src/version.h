36463e4eff3be1b6b1a14e8ffa929f4da3b32c58
raizor <raizor@gmail.com>
2018-12-10 20:19:30 +0000

    Update version.h

--- src/version.h
+++ src/version.h
@@ -9,16 +9,19 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70015;
+static const int PROTOCOL_VERSION = 60020; 
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 60018;
+
+// min protocol version that correctly supports PoS headers
+static const int VERSION_GETHEADERS_POS = 60020;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
+static const int GETHEADERS_VERSION = 60020;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
+static const int MIN_PEER_PROTO_VERSION = INIT_PROTO_VERSION;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -31,7 +34,7 @@ static const int BIP0031_VERSION = 60000;
 static const int NO_BLOOM_VERSION = 70011;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70012;
+static const int SENDHEADERS_VERSION = 60020;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
 static const int FEEFILTER_VERSION = 70013;
@@ -41,5 +44,9 @@ static const int SHORT_IDS_BLOCKS_VERSION = 70014;
 
 //! not banning for invalid compact blocks starts with this version
 static const int INVALID_CB_NO_BAN_VERSION = 70015;
+
+// only request blocks from nodes outside this range of versions
+static const int NOBLKS_VERSION_START = 0;
+static const int NOBLKS_VERSION_END = 60017;
 
 #endif // BITCOIN_VERSION_H
