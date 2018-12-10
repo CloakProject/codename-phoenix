8da5a28bca2f487f4ccaf4ca812d62405d69b5ee
raizor <raizor@gmail.com>
2018-12-10 20:19:30 +0000

    Update version.h

--- src/version.h
+++ src/version.h
@@ -12,10 +12,20 @@
 static const int PROTOCOL_VERSION = 70016;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 60018;
+
+// min protocol version that correctly supports PoS headers
+static const int VERSION_GETHEADERS_POS = 60020;
+
+//! In this version, 'getheaders' was introduced.
+static const int GETHEADERS_VERSION = 60020;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 31800;
+static const int MIN_PEER_PROTO_VERSION = INIT_PROTO_VERSION;
+
+//! nTime field added to CAddress, starting with this version;
+//! if possible, avoid requesting addresses nodes older than this
+static const int CADDR_TIME_VERSION = 31402;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
@@ -24,7 +34,7 @@ static const int BIP0031_VERSION = 60000;
 static const int NO_BLOOM_VERSION = 70011;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70012;
+static const int SENDHEADERS_VERSION = 60020;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
 static const int FEEFILTER_VERSION = 70013;
@@ -40,5 +50,8 @@ static const int WTXID_RELAY_VERSION = 70016;
 
 // Make sure that none of the values above collide with
 // `SERIALIZE_TRANSACTION_NO_WITNESS` or `ADDRV2_FORMAT`.
+// only request blocks from nodes outside this range of versions
+static const int NOBLKS_VERSION_START = 0;
+static const int NOBLKS_VERSION_END = 60017;
 
 #endif // BITCOIN_VERSION_H
