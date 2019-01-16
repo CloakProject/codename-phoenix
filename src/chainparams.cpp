f7798f4b67a51c9f141ee522f84f3527da7f5226
r3rcloak <codekicker@protonmail.com>
2019-01-16 10:54:17 +0000

    Remove extra trailing zeros from consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -106,7 +106,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1794700800000; // November 15th, 2026.
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1794700800; // November 15th, 2026.
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1826236800;      // November 15th, 2027.
 
         // The best chain should have at least this much work.
@@ -271,7 +271,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1794700800000; // November 15th, 2026.
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1794700800; // November 15th, 2026.
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1826236800;      // November 15th, 2027.
 
         // The best chain should have at least this much work.
