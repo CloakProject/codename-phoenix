850f5c65cef27dc02bad10e91e745798fa31d6ea
ikyokushin <vla.genya@mail.ru>
2021-02-18 12:30:34 +1000

    :zap: Add PoS placeholder and params. Update clientversion. Add PoS placeholder and params Update clientversion

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -83,6 +83,12 @@ struct Params {
     uint256 nMinimumChainWork;
     /** By default assume that the signatures in ancestors of this block are valid */
     uint256 defaultAssumeValid;
+	
+    // proof of stake
+    uint256 posLimit;
+    unsigned int nStakeMinAge;
+    unsigned int nStakeMaxAge;
+    unsigned int nStakeTargetSpacing;     
 
     /**
      * If true, witness commitments contain a payload equal to a Bitcoin Script solution
