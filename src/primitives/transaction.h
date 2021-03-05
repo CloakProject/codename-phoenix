4649e509548efe2489afb216cecbbe18295149b4
ikyokushin <vla.genya@mail.ru>
2021-03-05 11:15:53 +1000

    Update testnet port to match current client.

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -38,7 +38,6 @@ public:
 
     void SetNull() { hash.SetNull(); n = NULL_INDEX; }
     bool IsNull() const { return (hash.IsNull() && n == NULL_INDEX); }
-    bool GetCoinAge(uint64_t& nCoinAge) const;
 
     friend bool operator<(const COutPoint& a, const COutPoint& b)
     {
